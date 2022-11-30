acbba0ab23f8155daa078967a530d2d0baaf6d1c
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-12-01 08:30:00 +1300

    updated state for inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,28 @@
+import React, {useState} from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 // a compenent in react is just a javascript function
-
+//to update state in react you need to define the original element and then also the new updated element
+//and call it where it should replace the old element
 function ExpenseItem(props) {
+const [title, setTitle] = useState(props.title);
+console.log('ExpenseItem evaluated by React');
+
+ const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+ };
+
     return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
-        <div className="expense-item__description"><h2>{props.title}</h2>
+        <div className="expense-item__description"><h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Title</button>
     </Card>
     )
     }
